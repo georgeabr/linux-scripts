1e37acb4c206ce27c0d4597a13effe06a7aed12a
George Abraham <37941352+georgeabr@users.noreply.github.com>
2019-12-18 18:50:41 +0000

    Update zfs-ratio.sh

--- zfs-ratio.sh
+++ zfs-ratio.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# shows the compression ratio for each file in the specified folder
+# it adds a leading zero, if value less than 1
+# for example ./zfs-ratio.sh /bin/*
+# 1.00 - /bin/ypdomainname
+# 1.20 - /bin/zgrep
+# 0.40 - /bin/zless
+# 0.40 - /bin/zmore
+# 1.00 - /bin/znew
+
 for i in $* ; do
 actualsize=`du -s --apparent-size $i |awk '{print $1}'`
 compressedsize=`du -s $i |awk '{print $1}'`
