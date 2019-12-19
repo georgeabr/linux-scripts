eeac9f2850c166b421b72113a42aef6801ebf633
George Abraham <37941352+georgeabr@users.noreply.github.com>
2019-12-19 16:42:23 +0000

    Update zfs-ratio.sh

--- zfs-ratio.sh
+++ zfs-ratio.sh
@@ -12,6 +12,6 @@
 for i in $* ; do
 actualsize=`du -s --apparent-size $i |awk '{print $1}'`
 compressedsize=`du -s $i |awk '{print $1}'`
-var3=$(echo "scale=2;if (($actualsize/$compressedsize) < 1) print 0; print ($actualsize/$compressedsize)" | bc)
+var3=$(echo "scale=2;if (($actualsize/$compressedsize) < 1) print 0; if (($actualsize/$compressedsize) < 10) print 0; print ($actualsize/$compressedsize)" | bc)
 printf "$var3 - $i\n"
 done
