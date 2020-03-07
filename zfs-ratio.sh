f8a6e7ce91cd1f3155c6f4d98dc63ac4d9f4be8c
George Abraham <37941352+georgeabr@users.noreply.github.com>
2020-03-07 11:26:29 +0000

    Update zfs-ratio.sh

--- zfs-ratio.sh
+++ zfs-ratio.sh
@@ -2,16 +2,17 @@
 
 # shows the compression ratio for each file in the specified folder
 # it adds a leading zero, if value less than 1
-# for example ./zfs-ratio.sh /bin/*
-# 1.00 - /bin/ypdomainname
-# 1.20 - /bin/zgrep
-# 0.40 - /bin/zless
-# 0.40 - /bin/zmore
-# 1.00 - /bin/znew
+# for example: ./zfs-ratio.sh /opt/ /bin/ /var/ /usr/ /etc/ /boot/ 2>/dev/null
+# 01.51 - /opt/
+# 01.71 - /bin/
+# 01.08 - /var/
+# 01.40 - /usr/
+# 00.46 - /etc/
+# 01.03 - /boot/
 
 for i in $* ; do
-actualsize=`du -s --apparent-size $i 2>/dev/null|awk '{print $1}'`
-compressedsize=`du -s $i 2>/dev/null|awk '{print $1}'`
-var3=$(echo "scale=2;if (($actualsize/$compressedsize) < 1) print 0; if (($actualsize/$compressedsize) < 10) print 0; print ($actualsize/$compressedsize)" | bc)
-printf "$var3 - $i\n"
+  actualsize=`du -s --apparent-size $i 2>/dev/null|awk '{print $1}'`
+  compressedsize=`du -s $i 2>/dev/null|awk '{print $1}'`
+  var3=$(echo "scale=2;if (($actualsize/$compressedsize) < 1) print 0; if (($actualsize/$compressedsize) < 10) print 0; print ($actualsize/$compressedsize)" | bc)
+  printf "$var3 - $i\n"
 done
