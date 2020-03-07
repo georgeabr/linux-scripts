7ecc102ed4039195e01679dddb8fb82d8afcd181
George Abraham <37941352+georgeabr@users.noreply.github.com>
2020-03-07 11:38:31 +0000

    Update zfs-ratio.sh

--- zfs-ratio.sh
+++ zfs-ratio.sh
@@ -9,10 +9,20 @@
 # 01.40 - /usr/
 # 00.46 - /etc/
 # 01.03 - /boot/
+# or
+# 01.51 - /opt/ - 121M    /opt/
+# 01.71 - /bin/ - 182M    /bin/
+# 01.08 - /var/ - 2.9G    /var/
+# 01.40 - /usr/ - 5.7G    /usr/
+# 00.46 - /etc/ - 9.0M    /etc/
+# 01.03 - /boot/ - 199M   /boot/
+
 
 for i in $* ; do
   actualsize=`du -s --apparent-size $i 2>/dev/null|awk '{print $1}'`
   compressedsize=`du -s $i 2>/dev/null|awk '{print $1}'`
   var3=$(echo "scale=2;if (($actualsize/$compressedsize) < 1) print 0; if (($actualsize/$compressedsize) < 10) print 0; print ($actualsize/$compressedsize)" | bc)
-  printf "$var3 - $i\n"
+  # printf "$var3 - $i\n"
+  printf "$var3 - "; du -d0 -h $i 2>/dev/null
+
 done
