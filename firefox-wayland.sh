ca618e21a618cf9768b5bc54ff679c69099524e8
George Abraham <37941352+georgeabr@users.noreply.github.com>
2020-03-08 20:16:44 +0000

    Update firefox-wayland.sh

--- firefox-wayland.sh
+++ firefox-wayland.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #In KDE, copy paste from other apps to firefox-wayland or between firefox tabs DOES NOT WORK.
-# https://bugs.kde.org/show_bug.cgi?id=411682
+# https://bugs.kde.org/show_bug.cgi?id=411682 
 gsettings set org.gnome.desktop.interface cursor-size 32
 gsettings set org.gnome.desktop.interface cursor-blink false
 MOZ_ENABLE_WAYLAND=1 firefox-developer-edition
