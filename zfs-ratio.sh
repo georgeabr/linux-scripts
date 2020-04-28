6bbeef04dc6b560ce0f067c9c825955a113d003b
George Abraham <37941352+georgeabr@users.noreply.github.com>
2020-04-28 23:09:24 +0100

    Update zfs-ratio.sh

--- zfs-ratio.sh
+++ zfs-ratio.sh
@@ -3,17 +3,19 @@
 # shows the compression ratio for each file in the specified folder
 # it adds a leading zero, if value less than 1
 # for example: ./zfs-ratio.sh /opt/ /bin/ /var/ /usr/ /etc/ /boot/ 2>/dev/null
-# Ratio - size - name
-# ===================
-# 01.42 - 6.0G - /usr/
-# 01.26 - 1.5G - /var/
-# 01.86 - 2.5G - /lib/
+# Ratio     Size     Name
+# =========================
+# 01.42 -    6105M - /usr/
+# 01.26 -    1536M - /var/
+# 01.86 -    2495M - /lib/
 
-printf "Ratio - size - name\n"
-printf "===================\n"
+printf "Ratio     Size     Name\n"
+printf "=========================\n"
 for i in $* ; do
   actualsize=`du -s --apparent-size $i 2>/dev/null|awk '{print $1}'`
   compressedsize=`du -s $i 2>/dev/null|awk '{print $1}'`
   var3=$(echo "scale=2;if (($actualsize/$compressedsize) < 1) print 0; if (($actualsize/$compressedsize) < 10) print 0; print ($actualsize/$compressedsize)" | bc)
-  printf "$var3 - "; du -d0 -h $i 2>/dev/null| awk '{print $1 " - " $2}'
+  # printf "$var3 - "; du -d0 -BM $i 2>/dev/null| awk '{print $1 " - " $2}'
+  # awk '{ s = "00000000"$1; print substr(s, 1 + length(s) - 8); }' # 8 leading spaces
+  printf "$var3 - "; du -d0 -BM $i 2>/dev/null| awk '{s = "        "$1; print substr(s, 1 + length(s) - 8) " - " $2}'
 done
